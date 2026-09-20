b3283f4027be433d4993168c347577e580822f05
yuura <yuura.exe@gmail.com>
2026-09-20 22:08:46 +0900

    ✨ agent Actor詳細取得のHTTP配線を追加

--- src/server/http/actors.ts
+++ src/server/http/actors.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import {
   createAgentActor,
   createAgentActorInput,
+  getAgentActor,
   issueToken,
   listActorTokens,
   listActors,
@@ -15,12 +16,16 @@ import { idParam, zValidator } from './validator.js'
 
 export function actorRoutes(deps: RouteDeps) {
   const list = expose(listActors)
+  const get = expose(getAgentActor)
   const create = expose(createAgentActor)
   const updatePermissions = expose(updateAgentPermissions)
   const listTokens = expose(listActorTokens)
   const issue = expose(issueToken)
   return new Hono<ApiEnv>()
     .get('/', (c) => c.json(list(webCtx(deps, c), {}), 200))
+    .get('/:id', zValidator('param', idParam), (c) =>
+      c.json(get(webCtx(deps, c), c.req.valid('param')), 200),
+    )
     .post('/', zValidator('json', createAgentActorInput), (c) =>
       c.json(create(webCtx(deps, c), c.req.valid('json')), 200),
     )
